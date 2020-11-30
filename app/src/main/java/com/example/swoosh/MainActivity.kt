fe498c289975cbec7b86064cdb197f7204704eae
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-30 21:13:38 +0000

    Added the third layout

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
 
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Get the button event
-        val startingButton = findViewById<Button>(R.id.getStartedButton)
+        val startingButton = findViewById<Button>(R.id.finishButton)
 
         startingButton.setOnClickListener{
 
