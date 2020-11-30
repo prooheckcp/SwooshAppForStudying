8bd356f0a748e3eb4acadc60b7a6a8cc3e792dea
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-30 19:52:28 +0000

    Added intent

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Get the button event
+        val startingButton = findViewById<Button>(R.id.getStartedButton)
+
+        startingButton.setOnClickListener{
+
+            //Create the intent
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+
+            //Start the next intent
+            startActivity(leagueIntent)
+
+        }
+
     }
+
+
+
 }
